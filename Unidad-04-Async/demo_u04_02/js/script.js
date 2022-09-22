79560b6d78a5e4bfef79f59d164aac7872fbee5c
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2022-09-22 05:55:02 -0300

    ID-#15 Promesas Unidad 04

--- Unidad-04-Async/demo_u04_02/js/script.js
+++ Unidad-04-Async/demo_u04_02/js/script.js
@@ -14,12 +14,13 @@ new Promise(
         console.log(result);
         return result * 2;
     })
+    .then(result => {
+        console.log(result);
+        return result * 2;
+    })
 
 // 1) La promesa inicial se resuelve en 1 segundo(*)
 // 2) Entonces se llama al controlador .then en (**)
 // 3) El valor que devuelve 
 //             se pasa al siguiente controlador .then (***) 
-
-
-
 
