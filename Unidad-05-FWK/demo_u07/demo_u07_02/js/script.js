b1c30692400472a06cf30a4b1bd87b290eedc1cf
pabloin <pablo.eze.inchausti@gmail.com>
2022-11-03 10:23:55 -0300

    Ejemplo Clase 03-11-2022

--- Unidad-05-FWK/demo_u07/demo_u07_02/js/script.js
+++ Unidad-05-FWK/demo_u07/demo_u07_02/js/script.js
@@ -5,10 +5,20 @@ const Articulos = {
         <div>
             <router-link to="/articulos/1">Articulo 1</router-link>
             <router-link to="/articulos/2">Articulo 2</router-link>
+            <router-link to="/articulos/fruta">Manda Fruta</router-link>
         </div>
     `
 }
 const Articulo = { template: '<div>Estoy mostrando solo un Articulo</div>' }
+
+
+const Textaso = { template: `
+<img alt="Orgánico Diseño Receptivo" 
+class="media-image lazyload noversion visible" data-src="https://images01.nicepagecdn.com/page/43/32/es/plantilla-html-43328.jpg" src="https://images01.nicepagecdn.com/page/43/32/es/plantilla-html-43328.jpg"
+ style="max-width:100%;">
+` 
+}
+
 
 
 const routes = [
@@ -16,6 +26,7 @@ const routes = [
     { path: '/articulos',      component: Articulos },
     { path: '/compras',        component: Compras   },
     { path: '/articulos/:id',  component: Articulo  },
+    { path: '/textaso/:id',    component: Textaso  },
 ]
 
 const router = new VueRouter({
