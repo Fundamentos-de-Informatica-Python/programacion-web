b139ebfafa344e1c50110a1cddc11ed99c57c033
pabloin <pablo.eze.inchausti@gmail.com>
2022-11-03 10:11:04 -0300

    Ejemplo Clase 03-11-2022

--- Unidad-05-FWK/demo_u07/demo_u07_01/js/script.js
+++ Unidad-05-FWK/demo_u07/demo_u07_01/js/script.js
@@ -2,10 +2,17 @@ const Inicio    = { template: '<div>Estoy en el Inicio</div>'}
 const Articulos = { template: '<div>Estoy en la Lista de Articulos</div>' }
 const Compras   = { template: '<div>Estoy en el Comprando</div>' }
 
+
+
+const Ayuda   = { template: '<div>Lo que necesites: <br>tel:11 6888061 <br> mail:  super@rputer.com</div>' }
+const Store   = { template: '<div>Los mejores descuentos están aqui!!</div>' }
+
 const routes = [
     { path: '/inicio',    component: Inicio  },
     { path: '/articulos', component: Articulos },
-    { path: '/compras',   component: Compras }
+    { path: '/compras',   component: Compras },
+    { path: '/atencion-al-cliente',   component: Ayuda },
+    { path: '/tienda',   component: Store },
 ]
 
 const router = new VueRouter({
