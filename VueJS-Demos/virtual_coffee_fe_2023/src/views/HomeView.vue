c31fad926672daaf94c39fcf4b3d89144caa2d78
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2023-10-26 10:41:55 -0300

    vCafe 2023

---
+++ VueJS-Demos/virtual_coffee_fe_2023/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <Header/>
+
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "HomeView",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
